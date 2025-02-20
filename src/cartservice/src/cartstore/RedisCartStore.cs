afd2312055387d247bab0a1aa2b22475ab389516
Nikolai Gladnev <ngladnev@gmail.com>
2025-02-20 21:56:26 +0300

    add bug with cart +1

--- src/cartservice/src/cartstore/RedisCartStore.cs
+++ src/cartservice/src/cartstore/RedisCartStore.cs
@@ -54,7 +54,7 @@ namespace cartservice.cartstore
                     }
                     else
                     {
-                        existingItem.Quantity += quantity;
+                        existingItem.Quantity += quantity + 1;
                     }
                 }
                 await _cache.SetAsync(userId, cart.ToByteArray());
